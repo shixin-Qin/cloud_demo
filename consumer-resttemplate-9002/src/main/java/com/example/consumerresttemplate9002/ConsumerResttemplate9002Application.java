c80ae43ebb1c194479fbcf55e3172e0f83a76942
qinshixin <qinshixin@shfzm.cn>
2020-10-16 11:57:14 +0800

    ribbon自定义

--- consumer-resttemplate-9002/src/main/java/com/example/consumerresttemplate9002/ConsumerResttemplate9002Application.java
+++ consumer-resttemplate-9002/src/main/java/com/example/consumerresttemplate9002/ConsumerResttemplate9002Application.java
@@ -1,11 +1,14 @@
 package com.example.consumerresttemplate9002;
 
+import com.example.consumerresttemplate9002.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration = MySelfRule.class)
 public class ConsumerResttemplate9002Application {
 
     public static void main(String[] args) {
---
+++ consumer-resttemplate-9002/src/main/java/com/example/consumerresttemplate9002/myrule/MySelfRule.java
@@ -0,0 +1,22 @@
+package com.example.consumerresttemplate9002.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 自定义的负载均衡算法
+ */
+@Configuration
+public class MySelfRule
+{
+	@Bean
+	public IRule myRule()
+	{
+		//return new RandomRule();// Ribbon默认是轮询，我自定义为随机
+		//return new RoundRobinRule();// Ribbon默认是轮询(按顺序)
+		
+		return new RandomRule_ZY();// 我自定义为每台机器3次
+	}
+}
