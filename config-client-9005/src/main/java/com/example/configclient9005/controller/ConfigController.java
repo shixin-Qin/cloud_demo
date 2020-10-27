b74076f082cc0bc8295bcb550c02a574f695ecbe
qinshixin <qinshixin@shfzm.cn>
2020-10-27 17:43:41 +0800

    config-acu

--- config-client-9005/src/main/java/com/example/configclient9005/controller/ConfigController.java
+++ config-client-9005/src/main/java/com/example/configclient9005/controller/ConfigController.java
@@ -3,10 +3,12 @@ package com.example.configclient9005.controller;
 import com.example.configclient9005.configs.GitAutoRefreshConfig;
 import com.example.configclient9005.configs.GitConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
