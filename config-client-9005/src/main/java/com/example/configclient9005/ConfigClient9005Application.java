6c1db3348acc9fa1b6137143e544fc32000fcd99
qinshixin <qinshixin@shfzm.cn>
2020-10-27 17:10:30 +0800

    config-client

---
+++ config-client-9005/src/main/java/com/example/configclient9005/ConfigClient9005Application.java
@@ -0,0 +1,13 @@
+package com.example.configclient9005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient9005Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient9005Application.class, args);
+    }
+
+}
---
+++ config-client-9005/src/main/java/com/example/configclient9005/controller/ConfigController.java
@@ -0,0 +1,29 @@
+package com.example.configclient9005.controller;
+
+import com.example.configclient9005.configs.GitAutoRefreshConfig;
+import com.example.configclient9005.configs.GitConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Autowired
+    private GitConfig gitConfig;
+
+    @Autowired
+    private GitAutoRefreshConfig gitAutoRefreshConfig;
+
+    @GetMapping(value = "show")
+    public Object show(){
+        return gitConfig;
+    }
+
+    @GetMapping(value = "autoShow")
+    public Object autoShow(){
+        return gitAutoRefreshConfig;
+    }
+}
