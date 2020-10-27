a9ac7cade5add0cd85210453c315a1aef4615a8c
qinshixin <qinshixin@shfzm.cn>
2020-10-27 15:33:24 +0800

    config-single

---
+++ config-server-4003/src/main/java/com/example/configserver4003/ConfigServer4003Application.java
@@ -0,0 +1,15 @@
+package com.example.configserver4003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer4003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer4003Application.class, args);
+    }
+
+}
--- zuul-4001/src/main/java/com/example/zuul4001/filters/ManagerFilter.java
+++ zuul-4001/src/main/java/com/example/zuul4001/filters/ManagerFilter.java
@@ -35,3 +35,15 @@ public class ManagerFilter extends ZuulFilter {
     }
 
 }
+/**
+ * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过
+ * 滤器类型，具体如下：
+ * pre ：可以在请求被路由之前调用
+ * route ：在路由请求时候被调用
+ * post ：在route和error过滤器之后被调用
+ * error ：处理请求时发生错误时被调用
+ * filterOrder ：通过int值来定义过滤器的执行顺序
+ * shouldFilter ：返回一个boolean类型来判断该过滤器是否要执行，所以通过此函数可
+ * 实现过滤器的开关。在上例中，我们直接返回true，所以该过滤器总是生效
+ * run ：过滤器的具体逻辑。
+ */
